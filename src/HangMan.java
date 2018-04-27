c2c5289365dbb141be9e5e22d026f920f3b92494
League <league@WTS9.attlocal.net>
2018-04-26 18:56:52 -0700

    started it

---
+++ src/HangMan.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class HangMan {
+void start() {
+	String num = JOptionPane.showInputDialog("Choose a number");
+	int numOfWords = Integer.parseInt(num);
+}
+}
